ad0c03c142ce9acaa7aafafd30c4caea28604de0
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-01-20 22:23:48 +0000

    adds WaitFor and chromatic automation

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { rest } from "msw";
-import { fireEvent, within } from "@storybook/testing-library";
+import { fireEvent, within, waitFor } from "@storybook/testing-library";
 import InboxScreen from "./InboxScreen";
 import store from "../lib/store";
 
@@ -51,18 +51,17 @@ Default.parameters = {
     },
   },
 };
-// Function to emulate pausing between interactions
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+
 Default.play = async ({ canvasElement }) => {
-  sleep(3000);
   const canvas = within(canvasElement);
 
-  // Simulates pinning the first task
-  await fireEvent.click(canvas.getByLabelText("pinTask-1"));
-  // Simulates pinning the third task
-  await fireEvent.click(canvas.getByLabelText("pinTask-3"));
+  // Waits for the component to be updated based on the store
+  await waitFor(async () => {
+    // Simulates pinning the first task
+    await fireEvent.click(canvas.getByLabelText("pinTask-1"));
+    // Simulates pinning the third task
+    await fireEvent.click(canvas.getByLabelText("pinTask-3"));
+  });
 };
 
 export const Error = Template.bind({});
